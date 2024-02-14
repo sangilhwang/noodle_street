33386d64a38f0cde98e4aaa9d39fe69c369fe66f
Chris Hwang <sangil9959@gmail.com>
2024-02-14 11:48:33 +0900

    User 모델 수정 및 전체 모델 migrate
    User모델
    - AbstractUser 모델을 사용하도록 수정
    - 생일과 프로필 사진 항목 추가
    
    migration 적용

--- web/users/models.py
+++ web/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
-    
+class User(AbstractUser):
+    birthday = models.DateField("생년월일")
+    profile_image = models.ImageField("프로필 이미지", upload_to=None, blank=True)
