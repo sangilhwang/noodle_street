1f2e0abfbbf535649209fd6c698f14e77164ba06
Chris Hwang <sangil9959@gmail.com>
2024-02-15 11:18:44 +0900

    회원가입 기능 추가 및 로그인, 관리자 페이지 일부 수정
    signup 기능
    SignupForm() 추가
    signup_view 추가
    signup.html 추가 및 url 연결
    
    관리자 페이지에서 유저 정보를 확인 가능하도록 수정
    로그인 페이지의 title 문구 수정

--- web/users/urls.py
+++ web/users/urls.py
@@ -4,4 +4,6 @@ from users import views
 app_name = "users"
 urlpatterns = [
     path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
+    path("logout/", views.logout_view, name="logout"),
 ]
