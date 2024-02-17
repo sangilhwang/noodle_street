99e8a774f5c3d8b2087c9ea631d19876e3db140e
Jeong Woojeong <jaydenjeong218@gmail.com>
2024-02-17 15:33:04 +0900

    02.17 3:30pm 식당상세페이지 구현, html재사용 구현
    single_pages내에 restaurant_detail.html 생성 후 연결.
    base.html 생성 후 html 재사용 구현.
    main 페이지에 사진들 생성

--- web/single_pages/views.py
+++ web/single_pages/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from single_pages.models import Restraunt
 
 # Create your views here.
+
+def detail(request, restaurant_id):
+    restaurant = get_object_or_404(Restraunt, id=restaurant_id)  
+    #restaurant = Restraunt.objects.get(id=restaurant_id)
+    context = {
+        'restaurant':restaurant,
+    }
+    return render(request, 'single_pages/restaurant_detail.html', context)
